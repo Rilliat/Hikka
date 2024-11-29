d152047fd2cfa44bfc47a128b95201e28746e1d5
Rilliat <makarovskiyda@gmail.com>
2024-11-29 23:36:41 +0300

    ya ebal

--- hikka/inline/types.py
+++ hikka/inline/types.py
@@ -187,7 +187,7 @@ class BotInlineCall(CallbackQuery, BotInlineMessage):
     __pydantic_extra__: dict[str, Any] | None = _model_construction.NoInitField(init=False)
     chat_id: Optional[int] = Field(None)
     unit_id: Optional[str] = Field(None)
-    inline_manager: Optional["InlineManager"] = Field(None) # type: ignore  # noqa: F821
+    inline_manager: Optional[Any] = Field(None) # type: ignore  # noqa: F821
 
     def __init__(
         self,
