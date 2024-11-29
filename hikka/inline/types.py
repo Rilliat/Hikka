d6183cb4bf753a01c05aab0e3e88d7ad78e7e8ff
Rilliat <makarovskiyda@gmail.com>
2024-11-29 22:43:15 +0300

    ya ebal

--- hikka/inline/types.py
+++ hikka/inline/types.py
@@ -184,6 +184,7 @@ class BotInlineCall(CallbackQuery, BotInlineMessage):
     )
     __pydantic_extra__: dict[str, Any] | None = _model_construction.NoInitField(init=False)
     chat_id: Optional[int] = Field(None)
+    unit_id: Optional[str] = Field(None)
 
     def __init__(
         self,
