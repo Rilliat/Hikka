cd41e5dee0358cbeaf6c1ec616813d314fb3a731
Rilliat <makarovskiyda@gmail.com>
2024-12-16 09:00:42 +0300

    писюнчеке

--- hikka/log.py
+++ hikka/log.py
@@ -234,13 +234,11 @@ class TelegramLogsHandler(logging.Handler):
         if mod.db.get(__name__, "debugger", False):
             self.web_debugger = WebDebugger()
 
-        logging.error('L238 log.py')
         self._task = asyncio.create_task(self.queue_poller())
 
 
     async def queue_poller(self):
         while True:
-            logging.error('L243 log.py')
             with contextlib.suppress(Exception):
                 await self.sender()
             await asyncio.sleep(3)
@@ -363,37 +361,6 @@ class TelegramLogsHandler(logging.Handler):
                 for client_id in self._mods
             }
 
-            # self._exc_queue = {
-            #     client_id: [
-            #         item
-            #         for item in self.tg_buff
-            #         if isinstance(item[0], HikkaException)
-            #         and (not item[1] or item[1] == client_id or self.force_send_all)
-            #     ]
-            #     for client_id in self._mods
-            # }
-            #
-            # for client_id, exceptions in self._exc_queue.items():
-            #     for exc in exceptions:
-            #         await self._mods[client_id].inline.bot.send_message(
-            #             self._mods[client_id].logchat,
-            #             exc[0].message,
-            #             reply_markup=self._mods[client_id].inline.generate_markup(
-            #                 [
-            #                     {
-            #                         "text": "🪐 Full traceback",
-            #                         "callback": self._show_full_trace,
-            #                         "args": (
-            #                             self._mods[client_id].inline.bot,
-            #                             exc[0],
-            #                         ),
-            #                         "disable_security": True,
-            #                     },
-            #                     *self._gen_web_debug_button(exc[0]),
-            #                 ],
-            #             ),
-            #         )
-            logging.error('393 line log.py')
             self._exc_queue = {
                 str(client_id): [
                     self._mods[str(client_id)].inline.bot.send_message(
@@ -424,7 +391,6 @@ class TelegramLogsHandler(logging.Handler):
             for exceptions in self._exc_queue.values():
                 for exc in exceptions:
                     await exc
-                    logging.error('424 line log.py')
 
 
             self.tg_buff = []
